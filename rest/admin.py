930085f039d6e7fa28f1cdc17ccad895e4138989
Nikolas Erkinheimo <nikolas.erkinheimo@aalto.fi>
2018-02-03 16:58:52 +0200

    Alpha version of REST done. Things needed: -testing -comments -maybe more views
    PS. I wish this could have been done with the Django REST Framework. Sure, it is important for students to understand what is happening in the background but this method of learning carries the risk that the student will learn improper ways to deal with some problems.

--- rest/admin.py
+++ rest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from rest.models import ApiKey
 
 # Register your models here.
+
+admin.site.register(ApiKey)
--- rest/urls.py
+++ rest/urls.py
@@ -3,7 +3,8 @@ import rest.views as rest_views
 from django.http import JsonResponse
 
 urlpatterns = [
-    path('rest/games', rest_views.games, name='rest_games'),
+    path('rest/games', rest_views.all_games, name='rest_games'),
     path('rest/login', rest_views.rest_login, name='rest_login'),
     path('rest/logout', rest_views.rest_logout, name='rest_logout'),
+    path('rest/mygames', rest_views.user_games, name='user_games'),
 ]
